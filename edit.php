146d63ee33e18224ab95b888303bcc8f4043261d
siraphat16 <109897886+siraphat16@users.noreply.github.com>
2022-10-09 15:25:36 +0700

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,23 @@
+<?php include "connect.php" ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <?php
+
+     $stmt = $pdo->prepare("SELECT * FROM member");
+     $stmt->execute();
+
+     while ($row = $stmt->fetch()) {
+       echo "ชื่อสมาชิก:". $row ["name"] . "<br>";
+       echo "ที่อยู่:" . $row ["address"] . "<br>";
+       echo "อีเมล:".$row["email"]."<br>";
+       echo "<a href=workshop9.php?username=".$row["username"].">เเก้ไข</a>";
+       echo "<hr>\n";
+     } ?>
+   
+</body>
+</html>
